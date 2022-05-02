6b5216ad7e5288266c5ce58e3755adb4688533f1
avikam03 <avikam03@gmail.com>
2022-05-01 20:25:39 -0400

    made some design progress

--- App.js
+++ App.js
@@ -1,42 +1,103 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, SafeAreaView, TextInput, Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-function LoginScreen() {
-  const [text, onChangeText] = React.useState("Useless Text");
+
+function VerifyScreen({ navigation }) {
   const [number, onChangeNumber] = React.useState(null); 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-    </View>
+    <>
+    <StatusBar barStyle="light-content" />
+    <SafeAreaView style={styles.viewtemplate}>
+      <View style={styles.centerview}>
+        <Text style={{fontSize: 40, fontWeight: '700', color: 'white'}}>verify your phone number!</Text>
+        <TextInput
+          style={styles.datainput}
+          onChangeText={onChangeNumber}
+          autoFocus={true}
+          value={number}
+          placeholder="Enter the OTP you just received"
+          placeholderTextColor="#000" 
+          keyboardType="numeric"
+        />
+        <Pressable style={styles.button} onPress={() => navigation.navigate('Verify')}>
+          <Text style={styles.text}>Continue</Text>
+        </Pressable>
+      </View>
+      
+
+      
+
+    </SafeAreaView>
+    </>
   );
 }
 
+
+function LoginScreen({ navigation }) {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null); 
+  return (
+    <>
+    <StatusBar barStyle="light-content" />
+    <SafeAreaView style={styles.viewtemplate}>
+      <View style={styles.centerview}>
+        <Text style={{fontSize: 40, fontWeight: '700', color: 'white'}}>sign up / log in</Text>
+      </View>
+
+      <Text style={styles.titleinput}>Phone Number</Text>
+      
+      <View style={styles.centerview}>
+        <TextInput
+          style={styles.datainput}
+          onChangeText={onChangeNumber}
+          autoFocus={true}
+          value={number}
+          placeholder="Enter your Phone Number"
+          placeholderTextColor="#000" 
+        />
+        {/* <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        /> */}
+
+        <Pressable style={styles.button} onPress={() => navigation.navigate('Verify')}>
+          <Text style={styles.text}>Continue</Text>
+        </Pressable>
+
+      </View>
+
+    </SafeAreaView>
+    </>
+  );
+}
+
+var landingsvg = require ('./assets/landing.svg');
+
 function LandingScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>BookDon8</Text>
-      <Button
-        title="Get Started"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
+    <>
+    <StatusBar barStyle="light-content" />
+    <SafeAreaView style={styles.centerview}>
+
+      <Image source={landingsvg} />
+        <Text style={{fontSize: 40, fontWeight: '700'}}>
+          <Text style={{color: 'white'}}>Book</Text>
+          <Text style={{color: '#F9BC60'}}>Don8</Text>
+        </Text>
+        <Pressable style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.text}>Get Started</Text>
+        </Pressable>  
+    </SafeAreaView>
+    </>
   );
 }
 
@@ -45,9 +106,20 @@ function LandingScreen({ navigation }) {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Landing" component={LandingScreen} />
+      <Stack.Navigator
+      initialRouteName="Landing"
+      // screenOptions={({ route, navigation }) => ({
+      //   headerShown: false,
+      //   gestureEnabled: true,
+      // })}
+      >
+        <Stack.Screen name="Landing" component={LandingScreen} 
+        options={{
+          headerShown: false,
+        }}
+        />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Verify" component={VerifyScreen} />
       </Stack.Navigator>
     {/* <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -58,9 +130,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  viewtemplate: {
+    backgroundColor: '#004643',
     flex: 1,
-    backgroundColor: '#fff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  centerview: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    backgroundColor: '#004643',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -69,5 +149,48 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    marginHorizontal: '10%',
+    marginTop: '5%',
+    marginBottom: '10%'
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // width: '78%',
+    // height: '8%',
+    width: 306,
+    height: 59,
+    paddingVertical: 12,
+    paddingHorizontal: 70,
+    borderRadius: 15,
+    elevation: 3,
+    backgroundColor: '#F9BC60',
+    marginHorizontal: '10%',
+    marginVertical: '10%'
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
+  datainput: {
+    
+    // width: '78%',
+    // height: '8%',
+    width: 306,
+    height: 59,
+    backgroundColor: '#ABD1C6',
+    borderRadius: 15,
+    color: 'black',
+  },
+  titleinput: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 10,
+    marginHorizontal: '10%',
+  },
+
 });
